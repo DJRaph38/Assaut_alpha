d06f4eadd3b1bacc3dbc01b326522dd774c492c4
BuildTools <unconfigured@null.spigotmc.org>
2016-10-05 22:40:54 +0200

    // Added RivalrySpeedRecipes.java
    *EventsManager updated

--- Rivalry_alpha/src/fr/devcraftteam/events/EventsManager.java
+++ Rivalry_alpha/src/fr/devcraftteam/events/EventsManager.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 
 import fr.devcraftteam.rivalry.Main;
+import fr.devcraftteam.util.RivalrySpeedRecipes;
 
 public class EventsManager {
 
@@ -12,6 +13,7 @@ public class EventsManager {
         //EVENTS
         pm.registerEvents(new RivalryJoin(), pl); //Lorsqu'un joueur rejoins le serveur
         pm.registerEvents(new RivalryNoFall(), pl); //Lors du tp en début de jeu
+        pm.registerEvents(new RivalrySpeedRecipes(), pl);
     }//RegisterEvents
 
 }
---
+++ Rivalry_alpha/src/fr/devcraftteam/util/RivalrySpeedRecipes.java
@@ -0,0 +1,12 @@
+package fr.devcraftteam.util;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class RivalrySpeedRecipes implements Listener {
+	
+	@EventHandler
+	public void changeCraft(){
+		
+	}
+}
